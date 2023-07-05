2eaa23ea05eaff72a394f368bf7a651c694b76a4
Jac <jackieliu1998@gmail.com>
2023-07-05 00:30:39 -0400

    path to weights

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 
 #!/bin/bash
-nvcc -O3 utils.cu efficientnet.cpp main.cpp -I/usr/include/opencv4 -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lcudnn -lcublas -lcudart -o effnet && ./effnet /home/jg000/resnet/cudnn_effnet/weights
+echo "Enter path to weights folder:"
+read path
+echo "Compiling and running..."
+nvcc -O3 utils.cu efficientnet.cpp main.cpp -I/usr/include/opencv4 -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lcudnn -lcublas -lcudart -o effnet && ./effnet $path
 rm effnet
